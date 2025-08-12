92846436a677cfe990db8c4529f67b6b7d3485a6
Reinis Vimba <130878803+reinis-vimba@users.noreply.github.com>
2025-08-12 12:41:21 +0000

    add multiple question creation and validation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
     def new
-        respond_to do |format|
-            format.turbo_stream
+        quiz = Quiz.find(params[:quiz_id])
+        question = Question.new(quiz: quiz)
+        question.title = 'New Question'
+
+        if question.save
+            respond_to do |format|
+                format.turbo_stream { render turbo_stream: turbo_stream.append(:questions, partial: "questions/question", locals: { question: question, quiz: quiz }) }
+            end
         end
     end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+
+  validates :title, presence: true
 end
