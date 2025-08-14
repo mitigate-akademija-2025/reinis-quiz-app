dbac219b3306ac04508b1ef5e8b50e6d2e5654a5
Reinis Vimba <130878803+reinis-vimba@users.noreply.github.com>
2025-08-14 02:13:17 +0000

    refactor quiz controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -20,7 +20,15 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.new
     @quiz.title = 'New Quiz'
     @quiz.owner_id = current_user.id
-    @quiz.save
+    respond_to do |format|
+      if @quiz.save
+        format.html { redirect_to edit_quiz_path(@quiz), notice: "Quiz was successfully created." }
+        format.json { render :show, status: :created, location: @quiz }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @quiz.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /quizzes/1/edit
